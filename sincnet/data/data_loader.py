bb3c22f3cca0b0b85dee40bd54bea52897ff0576
Acyr Locatelli <acyr.locatelli86@gmail.com>
2020-10-18 20:08:25 +0100

    scale fix

--- sincnet/data/data_loader.py
+++ sincnet/data/data_loader.py
@@ -12,14 +12,13 @@ def data_loader(hparams, splits):
                                           with_info=True,
                                           download=True,
                                           as_supervised=True)
+    train = train.map(lambda x, y: (tf.cast(x, dtype=tf.float32)/32767.0, y),
     # apply augs here.
 
     train.shuffle(1000)
     train = train.padded_batch(hparams.batch_size,
-                               padded_shapes=((16000, ), ()),
-                               padding_values=tf.constant(0, dtype=tf.int64))
+                               padded_shapes=((16000, ), ()))
 
-    train = train.map(lambda x, y: (tf.cast(x, dtype=tf.float32)/32767.0, y),
                       num_parallel_calls=tf.data.experimental.AUTOTUNE)
     train = train.prefetch(tf.data.experimental.AUTOTUNE)
 
